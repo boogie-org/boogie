3150ac77d93f4279a9660ac61bab176602b22037
tabarbe <unknown>
2010-07-28 22:21:47 +0000

    Boogie: VCGeneration port part 2/3: Adding new dependent files, removing unnecessary one.

---
+++ Source/VCGeneration/version.cs
@@ -0,0 +1,8 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+using System.Reflection;
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
