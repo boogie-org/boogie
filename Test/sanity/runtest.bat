4a9e9a62ff2d9a530e56c8ed9c3f6bae2b9bf8d4
tabarbe <unknown>
2010-07-29 22:15:48 +0000

    Boogie: Added a new simple regression test, "sanity", which runs a single test for Boogie and a single test for Dafny, just to check for grievous runtime errors in the code.  (In my porting, I work with code that, in some cases, is not tested until the 3rd or 4th regression test.  These 2 test files should make use of that more obscure code and alert me to my errors quickly, rather than making me wait through a full regression cycle.)

---
+++ Test/sanity/runtest.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+set BOOGIEDIR=..\..\Binaries
+set BGEXE=%BOOGIEDIR%\Boogie.exe
+set DAFNY_EXE=%BOOGIEDIR%\Dafny.exe
+
+%BGEXE% %* /noVerify ..\textbook\bubble.bpl
+%DAFNY_EXE% /compile:0 %* ..\dafny1\Celebrity.dfy
