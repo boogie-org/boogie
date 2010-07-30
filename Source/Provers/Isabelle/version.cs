b16618051e08f0b286e9f2171bdf6ed5efa552b1
mikebarnett <unknown>
2010-07-30 04:12:05 +0000

    Made consistent the way all of the C# projects sign themselves and include the version information.

--- Source/Provers/Isabelle/version.cs
+++
@@ -1,8 +0,0 @@
-//-----------------------------------------------------------------------------
-//
-// Copyright (C) Microsoft Corporation.  All Rights Reserved.
-//
-//-----------------------------------------------------------------------------
-using System.Reflection;
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
