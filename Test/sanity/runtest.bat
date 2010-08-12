e7cf2c7763bb20e7090d3cc5f3f8fbe804a69453
tabarbe <unknown>
2010-08-12 22:30:57 +0000

    Boogie: This reg test was not running verification.

--- Test/sanity/runtest.bat
+++ Test/sanity/runtest.bat
@@ -5,5 +5,5 @@ set BOOGIEDIR=..\..\Binaries
 set BGEXE=%BOOGIEDIR%\Boogie.exe
 set DAFNY_EXE=%BOOGIEDIR%\Dafny.exe
 
-%BGEXE% %* /noVerify ..\textbook\bubble.bpl
+%BGEXE% %* ..\textbook\bubble.bpl
 %DAFNY_EXE% /compile:0 %* ..\dafny1\Celebrity.dfy
