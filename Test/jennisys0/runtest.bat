4c3d68142ca387c0cac5da85a6d3710455a5efe2
Rustan Leino <leino@microsoft.com>
2011-04-11 19:46:44 -0700

    Jennisys: First cut of injectivity analysis

--- Test/jennisys0/runtest.bat
+++ Test/jennisys0/runtest.bat
@@ -6,5 +6,5 @@ set JENNISYS_EXE=..\..\Jennisys\Jennisys\bin\Debug\Jennisys.exe
 for %%f in (Prog0.jen ExtensibleArray.jen) do (
   echo.
   echo -------------------- %%f --------------------
-  %JENNISYS_EXE% %* /trace %%f
+  %JENNISYS_EXE% %* /trace /noAnalysis %%f
 )
