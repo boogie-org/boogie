8b182b86b5fba577b7b441cb2029dabe082ae921
VccBuildServer <unknown>
2011-06-08 10:12:32 +0200

    Modifications of CC.NET build 2.1.30608.0

--- Source/version.cs
+++ Source/version.cs
@@ -1,8 +1,12 @@
-//-----------------------------------------------------------------------------
-//
-// Copyright (C) Microsoft Corporation.  All Rights Reserved.
-//
-//-----------------------------------------------------------------------------
+// ==++==
+// 
+//   
+// 
+// ==--==
+// Warning: Automatically generated file. DO NOT EDIT
+// Generated at Wednesday, June 08, 2011 10:11:43 AM
+
 using System.Reflection;
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.30608.0")]
+[assembly: AssemblyFileVersion("2.1.30608.0")]
+
