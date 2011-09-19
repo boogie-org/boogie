2750578c5d606e1cf447711bac1f3fe8d4a070ee
mschwerhoff <unknown>
2011-09-19 10:33:01 +0200

    Chalice: Modified chalice.bat s.t. it checks if all Java classpath elements exist

--- Chalice/chalice.bat
+++ Chalice/chalice.bat
@@ -1,5 +1,7 @@
 @echo off
+SetLocal EnableDelayedExpansion
 
+set ROOT_DIR=%~dp0
 set JAVA_EXE=java
 
 REM Attention: 'where' might not be available on all Windows versions
@@ -10,18 +12,33 @@ if not %ERRORLEVEL%==0 (
 )
 
 REM Get the Scala version, or rather, a string such as "scala-2.8.1"
-for /f "delims=" %%a in ('dir /b project\boot\scala-*') do @set SCALA_DIR=%%a
+for /f "delims=" %%A in ('dir /b %ROOT_DIR%\project\boot\scala-*') do @set SCALA_DIR=%%A
 
+REM Set classpath elements
+set __CP.SCALA_LIB=%ROOT_DIR%\project\boot\%SCALA_DIR%\lib\scala-library.jar
+set __CP.CHALICE=%ROOT_DIR%\target\%SCALA_DIR%.final\classes
+
+REM Assemble classpath and check if all classpath elements exist
 set CP=
-set CP=%CP%;project\boot\%SCALA_DIR%\lib\scala-library.jar
-set CP=%CP%;target\%SCALA_DIR%.final\classes
+for /f "tokens=2* delims=.=" %%A in ('set __CP.') do (
+	REM echo %%A %%B
+	if not exist %%B (
+		echo %%B does not exist.
+		goto :exit_with_error
+	) else (
+		set CP=!CP!;%%B
+	)
+)
 
+REM Chalice main class
 set CHALICE_MAIN=chalice.Chalice
 
+REM Chalice command line options
 set CHALICE_OPTS=
 set CHALICE_OPTS=%CHALICE_OPTS% /boogieOpt:nologo
 set CHALICE_OPTS=%CHALICE_OPTS% %*
 
+REM Assemble main command
 set CMD=%JAVA_EXE% -cp %CP% %CHALICE_MAIN% %CHALICE_OPTS%
 
 REM echo.
