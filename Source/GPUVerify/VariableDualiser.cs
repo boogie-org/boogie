6c8499199620f978003f186efd872e71177628f8
Unknown <t-alasdo@MSR-RISE-GUEST.redmond.corp.microsoft.com>
2011-09-29 14:37:40 -0700

    Added missing files

---
+++ Source/GPUVerify/VariableDualiser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Boogie;
+
+using System.Diagnostics;
+
+namespace GPUVerify
+{
+    class VariableDualiser : Duplicator
+    {
+        private int id;
+
+        public VariableDualiser(int id)
+        {
+            this.id = id;
+        }
+
+        public override Expr VisitIdentifierExpr(IdentifierExpr node)
+        {
+            if (!(node.Decl is Constant))
+            {
+                return new IdentifierExpr(node.tok, new LocalVariable(node.tok, new TypedIdent(node.tok, node.Decl.Name + "$" + id, node.Decl.TypedIdent.Type)));
+            }
+
+            return node;
+        }
+
+
+        public override Variable VisitVariable(Variable node)
+        {
+            if (!(node is Constant))
+            {
+                node.TypedIdent = new TypedIdent(node.tok, node.Name + "$" + id, node.TypedIdent.Type);
+                node.Name = node.Name + "$" + id;
+                return node;
+            }
+
+            return base.VisitVariable(node);
+        }
+
+
+    }
+
+}
