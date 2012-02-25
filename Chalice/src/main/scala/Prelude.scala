ab6afd0af9811e6c3c292fa7302dc65beff564fc
stefanheule <unknown>
2012-02-25 03:12:33 -0800

    Chalice: unneeded type parameter to quantifier

--- Chalice/src/main/scala/Prelude.scala
+++ Chalice/src/main/scala/Prelude.scala
@@ -199,7 +199,7 @@ function IsGoodExhaleState(eh: HeapType, h: HeapType,
   (forall o: ref :: { h[o, held] }  (0<h[o, held]) ==> eh[o, mu] == h[o, mu]) &&
   (forall o: ref :: { h[o, rdheld] }  h[o, rdheld] ==> eh[o, mu] == h[o, mu]) &&
   (forall o: ref :: { h[o, forkK] } { eh[o, forkK] } h[o, forkK] == eh[o, forkK]) &&
-  (forall<T> o: ref, f: Field int :: { eh[o, f], PredicateField(f) } PredicateField(f) ==> h[o, f] <= eh[o, f])
+  (forall o: ref, f: Field int :: { eh[o, f], PredicateField(f) } PredicateField(f) ==> h[o, f] <= eh[o, f])
 }"""
 }
 object PermissionFunctionsAndAxiomsPL extends PreludeComponent {
