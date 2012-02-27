3cbf1ebd6dfd5b2aa3a93d0b682ad5c13f911177
Unknown <stefan@stefan-mobile.dhcp4.washington.edu>
2012-02-27 03:52:57 -0800

    Chalice: (change indendation of source file)

--- Chalice/src/main/scala/Ast.scala
+++ Chalice/src/main/scala/Ast.scala
@@ -873,75 +873,75 @@ object AST {
   // Applies recursively the function f first to the expression and, if f returns true, then to its subexpressions
   def visitOpt(expr: RValue, f: RValue => Boolean) {
     if (f(expr)) {
-    expr match {
-     case _:Literal => ;
-     case _:ThisExpr => ;
-     case _:Result => ;
-     case _:VariableExpr => ;
-     case _:BoogieExpr => ;
-     case MemberAccess(e, _) =>
-       visitOpt(e, f);
-
-     case Frac(p) => visitOpt(p, f);
-     case Epsilons(p) => visitOpt(p, f);
-     case Full | Epsilon | Star | MethodEpsilon =>;
-     case ChannelEpsilon(None) | PredicateEpsilon(None) | MonitorEpsilon(None) =>;
-     case ChannelEpsilon(Some(e)) => visitOpt(e, f);
-     case PredicateEpsilon(Some(e)) => visitOpt(e, f);
-     case MonitorEpsilon(Some(e)) => visitOpt(e, f);
-     case ForkEpsilon(tk) => visitOpt(tk, f);
-     case IntPermTimes(n, p) =>
-       visitOpt(n, f); visitOpt(p, f);
-     case PermTimes(e0, e1) =>
-       visitOpt(e0, f); visitOpt(e1, f);
-     case PermPlus(e0, e1) =>
-       visitOpt(e0, f); visitOpt(e1, f);
-     case PermMinus(e0, e1) =>
-       visitOpt(e0, f); visitOpt(e1, f);
-     case Access(e, perm) =>
-       visitOpt(e, f); visitOpt(perm, f);
-     case AccessAll(obj, perm) =>
-       visitOpt(obj, f); visitOpt(perm, f);
-     case AccessSeq(s, _, perm) =>
-       visitOpt(s, f); visitOpt(perm, f);
-
-     case Credit(e, n) =>
-       visitOpt(e, f); n match { case Some(n) => visitOpt(n, f); case _ => }
-     case Holds(e) => visitOpt(e, f);
-     case RdHolds(e) => visitOpt(e, f);
-
-     case e: BinaryExpr =>
-       visitOpt(e.E0, f); visitOpt(e.E1, f);
-     case Range(min, max) =>
-       visitOpt(min, f); visitOpt(max, f);
-     case e: Assigned => e
-     case Old(e) => visitOpt(e, f);
-     case IfThenElse(con, then, els) => visitOpt(con, f); visitOpt(then, f); visitOpt(els, f);
-     case Not(e) => visitOpt(e, f);
-     case funapp@FunctionApplication(obj, id, args) =>
-       visitOpt(obj, f); args foreach { arg => visitOpt(arg, f) };
-     case Unfolding(pred, e) =>
-       visitOpt(pred, f); visitOpt(e, f);
-
-     case SeqQuantification(_, _, seq, e) => visitOpt(seq, f); visitOpt(e, f);
-     case TypeQuantification(_, _, _, e, (min,max)) => visitOpt(e, f); visitOpt(min, f); visitOpt(max, f);
-     case TypeQuantification(_, _, _, e, _) => visitOpt(e, f);
-     case ExplicitSeq(es) =>
-       es foreach { e => visitOpt(e, f) }
-     case Length(e) =>
-       visitOpt(e, f)
-     case Eval(h, e) =>
-       h match {
-         case AcquireState(obj) => visitOpt(obj, f);
-         case ReleaseState(obj) => visitOpt(obj, f);
-         case CallState(token, obj, id, args) =>
-           visitOpt(token, f); visitOpt(obj, f); args foreach {a : Expression => visitOpt(a, f)};
-       }
-       visitOpt(e, f);
-     case NewRhs(_, init, lowerBounds, upperBounds) =>
-       lowerBounds foreach { e => visitOpt(e, f)};
-       upperBounds foreach { e => visitOpt(e, f)};
-   }
+      expr match {
+         case _:Literal => ;
+         case _:ThisExpr => ;
+         case _:Result => ;
+         case _:VariableExpr => ;
+         case _:BoogieExpr => ;
+         case MemberAccess(e, _) =>
+           visitOpt(e, f);
+         
+         case Frac(p) => visitOpt(p, f);
+         case Epsilons(p) => visitOpt(p, f);
+         case Full | Epsilon | Star | MethodEpsilon =>;
+         case ChannelEpsilon(None) | PredicateEpsilon(None) | MonitorEpsilon(None) =>;
+         case ChannelEpsilon(Some(e)) => visitOpt(e, f);
+         case PredicateEpsilon(Some(e)) => visitOpt(e, f);
+         case MonitorEpsilon(Some(e)) => visitOpt(e, f);
+         case ForkEpsilon(tk) => visitOpt(tk, f);
+         case IntPermTimes(n, p) =>
+           visitOpt(n, f); visitOpt(p, f);
+         case PermTimes(e0, e1) =>
+           visitOpt(e0, f); visitOpt(e1, f);
+         case PermPlus(e0, e1) =>
+           visitOpt(e0, f); visitOpt(e1, f);
+         case PermMinus(e0, e1) =>
+           visitOpt(e0, f); visitOpt(e1, f);
+         case Access(e, perm) =>
+           visitOpt(e, f); visitOpt(perm, f);
+         case AccessAll(obj, perm) =>
+           visitOpt(obj, f); visitOpt(perm, f);
+         case AccessSeq(s, _, perm) =>
+           visitOpt(s, f); visitOpt(perm, f);
+         
+         case Credit(e, n) =>
+           visitOpt(e, f); n match { case Some(n) => visitOpt(n, f); case _ => }
+         case Holds(e) => visitOpt(e, f);
+         case RdHolds(e) => visitOpt(e, f);
+         
+         case e: BinaryExpr =>
+           visitOpt(e.E0, f); visitOpt(e.E1, f);
+         case Range(min, max) =>
+           visitOpt(min, f); visitOpt(max, f);
+         case e: Assigned => e
+         case Old(e) => visitOpt(e, f);
+         case IfThenElse(con, then, els) => visitOpt(con, f); visitOpt(then, f); visitOpt(els, f);
+         case Not(e) => visitOpt(e, f);
+         case funapp@FunctionApplication(obj, id, args) =>
+           visitOpt(obj, f); args foreach { arg => visitOpt(arg, f) };
+         case Unfolding(pred, e) =>
+           visitOpt(pred, f); visitOpt(e, f);
+         
+         case SeqQuantification(_, _, seq, e) => visitOpt(seq, f); visitOpt(e, f);
+         case TypeQuantification(_, _, _, e, (min,max)) => visitOpt(e, f); visitOpt(min, f); visitOpt(max, f);
+         case TypeQuantification(_, _, _, e, _) => visitOpt(e, f);
+         case ExplicitSeq(es) =>
+           es foreach { e => visitOpt(e, f) }
+         case Length(e) =>
+           visitOpt(e, f)
+         case Eval(h, e) =>
+           h match {
+             case AcquireState(obj) => visitOpt(obj, f);
+             case ReleaseState(obj) => visitOpt(obj, f);
+             case CallState(token, obj, id, args) =>
+               visitOpt(token, f); visitOpt(obj, f); args foreach {a : Expression => visitOpt(a, f)};
+           }
+           visitOpt(e, f);
+         case NewRhs(_, init, lowerBounds, upperBounds) =>
+           lowerBounds foreach { e => visitOpt(e, f)};
+           upperBounds foreach { e => visitOpt(e, f)};
+     }
    }
  }
 }
