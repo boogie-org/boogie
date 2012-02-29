8f2f6d4a5a63a2e817e76422ebbab878a940351f
Unknown <afd@afd-THINK.home>
2012-02-29 20:42:35 +0000

    Added missing files.

---
+++ Source/GPUVerify/CrossThreadInvariantProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Boogie;
+using System.Diagnostics;
+
+namespace GPUVerify
+{
+    class CrossThreadInvariantProcessor : StandardVisitor
+    {
+
+        public override Expr VisitNAryExpr(NAryExpr node)
+        {
+            
+            if (node.Fun is FunctionCall)
+            {
+                FunctionCall call = node.Fun as FunctionCall;
+
+                if (call.Func.Name.Equals("__uniform_bv32") || call.Func.Name.Equals("__uniform_bool"))
+                {
+                    return Expr.True;
+/*                    Debug.Assert(false);
+                    return Expr.Eq(new VariableDualiser(1).VisitExpr(node.Args[0].Clone() as Expr),
+                                   new VariableDualiser(2).VisitExpr(node.Args[0].Clone() as Expr));*/
+                }
+
+                if (call.Func.Name.Equals("__distinct_bv32") || call.Func.Name.Equals("__distinct_bool"))
+                {
+                    return Expr.Neq(new VariableDualiser(1).VisitExpr(node.Args[0].Clone() as Expr),
+                                   new VariableDualiser(2).VisitExpr(node.Args[0].Clone() as Expr));
+                }
+
+            }
+
+            return base.VisitNAryExpr(node);
+        }
+
+
+
+    }
+}
