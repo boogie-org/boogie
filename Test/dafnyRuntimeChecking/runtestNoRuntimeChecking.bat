c0f50ec423987b670181d13a416c1bffd279556a
chmaria <unknown>
2012-06-18 04:57:09 +0200

    Dafny: Added CC translation of postconditions.

--- Test/dafnyRuntimeChecking/runtestNoRuntimeChecking.bat
+++ Test/dafnyRuntimeChecking/runtestNoRuntimeChecking.bat
@@ -6,7 +6,7 @@ set DAFNY_EXE=%BOOGIEDIR%\Dafny.exe
 set BPLEXE=%BOOGIEDIR%\Boogie.exe
 
 for %%f in (AssumeStmt0 AssumeStmt1 AssertStmt0 AssertStmt1
-    Precondition0 Precondition1) do (
+    Precondition0 Precondition1 Postcondition0 Postcondition1) do (
   echo.
   echo -------------------- %%f --------------------
   %DAFNY_EXE% /nologo /errorTrace:0 /verification:0 /runtimeChecking:0 /compile:2 %* %%f.dfy
