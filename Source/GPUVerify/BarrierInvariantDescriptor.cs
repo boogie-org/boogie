00ece9690862b315ac57c45c1dfb066f5d53b4cb
Unknown <afd@afd-THINK.home>
2012-09-26 20:04:50 +0100

    Barrier invariants can now refer to local variables that are uniform.

--- Source/GPUVerify/BarrierInvariantDescriptor.cs
+++ Source/GPUVerify/BarrierInvariantDescriptor.cs
@@ -10,15 +10,21 @@ namespace GPUVerify {
 
     protected Expr Predicate;
     protected Expr BarrierInvariant;
+    protected QKeyValue SourceLocationInfo;
     protected KernelDualiser Dualiser;
+    protected string ProcName;
 
-    public BarrierInvariantDescriptor(Expr Predicate, Expr BarrierInvariant, KernelDualiser Dualiser) {
+    public BarrierInvariantDescriptor(Expr Predicate, Expr BarrierInvariant,
+          QKeyValue SourceLocationInfo,
+          KernelDualiser Dualiser, string ProcName) {
       this.Predicate = Predicate;
       this.BarrierInvariant = BarrierInvariant;
+      this.SourceLocationInfo = SourceLocationInfo;
       this.Dualiser = Dualiser;
+      this.ProcName = ProcName;
     }
 
-    internal abstract AssertCmd GetAssertCmd(QKeyValue Attributes);
+    internal abstract AssertCmd GetAssertCmd();
 
     internal abstract List<AssumeCmd> GetInstantiationCmds();
 
