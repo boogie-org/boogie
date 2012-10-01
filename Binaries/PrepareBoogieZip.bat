247a9f10e453d7ab84fd77422ccce1a021d4fc54
wuestholz <unknown>
2012-10-01 10:04:32 +0200

    Updated the 'PrepareBoogieZip.bat' script.

--- Binaries/PrepareBoogieZip.bat
+++ Binaries/PrepareBoogieZip.bat
@@ -7,33 +7,28 @@ if exist %DEST_DIR% del /q %DEST_DIR%\*
 if not exist %DEST_DIR% mkdir %DEST_DIR%
 
 for %%f in (
-  AbsInt.dll                  AbsInt.pdb
-  AIFramework.dll             AIFramework.pdb
-  Basetypes.dll               Basetypes.pdb
-  Boogie.exe                  Boogie.pdb
-  BVD.exe                     BVD.pdb
-  CodeContractsExtender.dll   CodeContractsExtender.pdb
-  Core.dll                    Core.pdb
-  Dafny.exe                   Dafny.pdb
-  DafnyPipeline.dll           DafnyPipeline.pdb
-  Graph.dll                   Graph.pdb
-  Houdini.dll                 Houdini.pdb
-  Model.dll                   Model.pdb
-  ParserHelper.dll            ParserHelper.pdb
-  Provers.Isabelle.dll        Provers.Isabelle.pdb
-  Provers.Simplify.dll        Provers.Simplify.pdb
-  Provers.SMTLib.dll          Provers.SMTLib.pdb
-  Provers.TPTP.dll            Provers.TPTP.pdb
-  Provers.Z3.dll              Provers.Z3.pdb
-  VCExpr.dll                  VCExpr.pdb
-  VCGeneration.dll            VCGeneration.pdb
-  DafnyPrelude.bpl
-  DafnyRuntime.cs
+  AbsInt.dll                          AbsInt.pdb
+  Basetypes.dll                       Basetypes.pdb
+  Boogie.exe                          Boogie.pdb
+  CodeContractsExtender.dll           CodeContractsExtender.pdb
+  Core.dll                            Core.pdb
+  Dafny.exe                           Dafny.pdb
+  DafnyPrelude.bpl                    DafnyRuntime.cs
+  DafnyPipeline.dll                   DafnyPipeline.pdb
+  Graph.dll                           Graph.pdb
+  Houdini.dll
+  Model.dll                           Model.pdb
+  ParserHelper.dll                    ParserHelper.pdb
+  Provers.SMTLib.dll                  Provers.SMTLib.pdb
   TypedUnivBackPred2.sx
-  UnivBackPred2.smt2
+  UnivBackPred2.smt                   UnivBackPred2.smt2
   UnivBackPred2.sx
+  VCExpr.dll                          VCExpr.pdb
+  VCGeneration.dll                    VCGeneration.pdb
 ) do (
   copy %%f %DEST_DIR%
 )
+
+xcopy /E /I /Y CodeContracts "%DEST_DIR%/CodeContracts"
 
 echo Done.  Now, manually put the contents of the %DEST_DIR% directory into Boogie.zip
