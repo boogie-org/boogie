2843041a65b01035d933f4fb8a5690f1c92285be
akashlal <unknown>
2013-03-11 09:45:07 +0530

    Added another constructor to CallCmd

--- Source/Core/AbsyCmd.cs
+++ Source/Core/AbsyCmd.cs
@@ -1798,6 +1798,19 @@ namespace Microsoft.Boogie {
       this.Outs = outs;
     }
 
+    public CallCmd(IToken tok, string callee, List<Expr> ins, List<IdentifierExpr> outs, QKeyValue kv, bool IsAsync)
+        : base(tok, kv)
+    {
+        Contract.Requires(outs != null);
+        Contract.Requires(ins != null);
+        Contract.Requires(callee != null);
+        Contract.Requires(tok != null);
+        this.callee = callee;
+        this.Ins = ins;
+        this.Outs = outs;
+        this.IsAsync = IsAsync;
+    }
+
     public CallCmd(IToken tok, string callee, List<Expr> ins, List<IdentifierExpr> outs, QKeyValue kv, CallCmd inParallelWith)
         : base(tok, kv)
     {
