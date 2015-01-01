820bde31cbd5526f56bbe8d0bde2b37812b25b3d
0biha <unknown>
2015-01-01 17:23:10 +0100

    Made invariant of class 'StructuredCmd' robust by changing the design (replaced public field by private field + getter/setter)

--- Source/Core/AbsyCmd.cs
+++ Source/Core/AbsyCmd.cs
@@ -705,15 +705,30 @@ namespace Microsoft.Boogie {
 
   [ContractClass(typeof(StructuredCmdContracts))]
   public abstract class StructuredCmd {
-    public IToken/*!*/ tok;
+    private IToken/*!*/ _tok;
+
+    public IToken/*!*/ tok
+    {
+      get
+      {
+        Contract.Ensures(Contract.Result<IToken>() != null);
+        return this._tok;
+      }
+      set
+      {
+        Contract.Requires(value != null);
+        this._tok = value;
+      }
+    }
+
     [ContractInvariantMethod]
     void ObjectInvariant() {
-      Contract.Invariant(tok != null);
+      Contract.Invariant(this._tok != null);
     }
 
     public StructuredCmd(IToken tok) {
       Contract.Requires(tok != null);
-      this.tok = tok;
+      this._tok = tok;
     }
 
     public abstract void Emit(TokenTextWriter/*!*/ stream, int level);
