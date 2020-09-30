5d28a188c25902fc0229897c83525d3cccad26fe
Shaz Qadeer <shaz@fb.com>
2020-09-29 17:12:14 -0700

    added fixed random seed to reduce flakiness

--- Test/civl/inductive-sequentialization/paxos/is.sh
+++ Test/civl/inductive-sequentialization/paxos/is.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# RUN: %boogie -useArrayTheory Paxos.bpl PaxosSeqAxioms.bpl PaxosActions.bpl PaxosAbstractions.bpl PaxosSeq.bpl > "%t"
+# RUN: %boogie -useArrayTheory Paxos.bpl PaxosSeqAxioms.bpl PaxosActions.bpl PaxosAbstractions.bpl PaxosSeq.bpl -proverOpt:O:smt.random_seed=100 > "%t"
 # RUN: %diff "%s.expect" "%t"
 
 boogie -nologo -useArrayTheory $@ Paxos.bpl PaxosSeqAxioms.bpl PaxosActions.bpl PaxosAbstractions.bpl PaxosSeq.bpl
