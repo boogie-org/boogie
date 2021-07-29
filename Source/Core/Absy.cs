935fcee08bf7bdc1f6f922d14be2ab1310987ea1
Jatin Arora <jatina29@gmail.com>
2021-07-29 15:39:17 -0700

    free && assume

--- Source/Core/Absy.cs
+++ Source/Core/Absy.cs
@@ -3555,7 +3555,7 @@ namespace Microsoft.Boogie
 
     public bool CanAlwaysAssume()
     {
-      return QKeyValue.FindBoolAttribute(Attributes, "always_assume");
+      return Free && QKeyValue.FindBoolAttribute(Attributes, "always_assume");
     }
 
 
@@ -3684,7 +3684,7 @@ namespace Microsoft.Boogie
 
     public bool CanAlwaysAssume ()
     {
-      return QKeyValue.FindBoolAttribute(this.Attributes, "always_assume");
+      return Free && QKeyValue.FindBoolAttribute(this.Attributes, "always_assume");
     }
 
     public Ensures(IToken token, bool free, Expr /*!*/ condition, string comment, QKeyValue kv)
