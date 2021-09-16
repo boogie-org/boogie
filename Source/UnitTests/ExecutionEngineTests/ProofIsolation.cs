f397542bd6604b30b8467d24dbdcdf63d2fc3487
Remy Willems <>
2021-09-16 17:24:39 +0200

    Further improve test so it exposes a generation problem with the unique constants

--- Source/UnitTests/ExecutionEngineTests/ProofIsolation.cs
+++ Source/UnitTests/ExecutionEngineTests/ProofIsolation.cs
@@ -45,6 +45,93 @@ procedure N(x: int)
       var proverLog3 = GetProverLogForProgram(procedure2And1).ToList();
       Assert.AreEqual(proverLog3[0], proverLog2[0]);
     }
+    
+    [Test()]
+    public void Implementation2()
+    {
+      var procedure1 = @"
+type Wicket;
+const w : Wicket;
+function age(Wicket) returns (int);
+axiom age(w) == 7;
+
+var favorite: Wicket;
+
+type Barrel a;
+type RGBColor;
+const unique red: RGBColor;
+const unique green: RGBColor;
+const unique blue: RGBColor;
+axiom ( forall ce: RGBColor :: ce == red || ce == green || ce == blue );
+
+const m: [Barrel Wicket] Wicket;
+const n: <a> [Barrel a] a;
+
+type MySynonym a = int;
+type ComplicatedInt = MySynonym (MySynonym bool);
+
+procedure M(x: int, coloredBarrel: Barrel RGBColor)
+  requires x == 2; 
+  modifies favorite;
+  ensures age(favorite) == 42; 
+{
+  var y: ComplicatedInt;
+  assert y == 3;
+}
+";
+      
+      var procedure2 = @"
+type Wicket2;
+const w2 : Wicket2;
+function age2(Wicket2) returns (int);
+axiom age2(w2) == 7;
+
+var favorite2: Wicket2;
+
+type Barrel2 a;
+type RGBColor2;
+const unique red2: RGBColor2;
+const unique green2: RGBColor2;
+const unique blue2: RGBColor2;
+axiom ( forall ce2: RGBColor2 :: ce2 == red2 || ce2 == green2 || ce2 == blue2 );
+
+const m2: [Barrel2 Wicket2] Wicket2;
+const n2: <a> [Barrel2 a] a;
+
+type MySynonym2 a = int;
+type ComplicatedInt2 = MySynonym2 (MySynonym2 bool);
+
+procedure M2(x: int, coloredBarrel: Barrel2 RGBColor2)
+  requires x == 2; 
+  modifies favorite2;
+  ensures age2(favorite2) == 42; 
+{
+  var y: ComplicatedInt2;
+  assert y == 3;
+}
+";
+      
+      var procedure1And2 = $@"
+{procedure1}
+{procedure2}";
+      
+      var procedure2And1 = $@"
+{procedure1}
+{procedure2}";
+      CommandLineOptions.Install(new CommandLineOptions());
+      CommandLineOptions.Clo.PruneFunctionsAndAxioms = true;
+      CommandLineOptions.Clo.Parse(new string[]{});
+      ExecutionEngine.printer = new ConsolePrinter();
+      
+      var proverLog1 = GetProverLogForProgram(procedure1).ToList();
+      CommandLineOptions.Clo.ProcsToCheck.Add("M");
+      var proverLog2 = GetProverLogForProgram(procedure1And2).ToList();
+      Assert.AreEqual(proverLog1.Count, 1);
+      Assert.AreEqual(proverLog1.Count, proverLog2.Count);
+      Assert.AreEqual(proverLog1[0], proverLog2[0]);
+      var proverLog3 = GetProverLogForProgram(procedure2And1).ToList();
+      Assert.AreEqual(proverLog3[0], proverLog2[0]);
+    }
 
     private static IEnumerable<string> GetProverLogForProgram(string procedure1)
     {
