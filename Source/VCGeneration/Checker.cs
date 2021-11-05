d1ef8cb99a888d82cf60952d101a3839633a0720
Remy Willems <>
2021-11-05 16:05:37 +0100

    Add missing using

--- Source/VCGeneration/Checker.cs
+++ Source/VCGeneration/Checker.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Linq;
 using Microsoft.Boogie.VCExprAST;
 using System.Threading.Tasks;
 using VC;
