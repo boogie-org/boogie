24c7e2ac0d91bacc52f7696c491bf5b9a752e467
Remy Willems <>
2021-11-12 12:23:26 +0100

    Removed unused import

--- Source/VCGeneration/Checker.cs
+++ Source/VCGeneration/Checker.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using System.Linq;
 using Microsoft.Boogie.VCExprAST;
 using System.Threading.Tasks;
 using VC;
