116097b29342a324e59371ecc0cd1e32e4f2103f
Remy Willems <>
2021-11-26 13:40:59 +0100

    Enable include_dep attribute

--- Source/VCGeneration/Prune/DependencyEvaluator.cs
+++ Source/VCGeneration/Prune/DependencyEvaluator.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Boogie
     protected void AddIncoming(Declaration newIncoming)
     {
       if (!ExcludeDep(newIncoming)) {
-        if (CommandLineOptions.Clo.Prune == CommandLineOptions.PruneMode.Automatic) {
+        if (QKeyValue.FindBoolAttribute(declaration.Attributes, "include_dep") || CommandLineOptions.Clo.Prune == CommandLineOptions.PruneMode.Automatic) {
           incomingSets.Add(new[] { newIncoming });
         }
       }
