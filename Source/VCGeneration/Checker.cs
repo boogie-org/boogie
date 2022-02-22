8a133e4108f07c1c139445e755682dfaffbdc82d
Remy Willems <rwillems@amazon.com>
2022-02-22 21:35:49 +0100

    Fix for models

--- Source/VCGeneration/Checker.cs
+++ Source/VCGeneration/Checker.cs
@@ -61,6 +61,11 @@ namespace Microsoft.Boogie
     public void GoBackToIdle()
     {
       Contract.Requires(IsBusy);
+      if (Options.ModelViewFile != null) {
+        // Don't re-use theorem provers whose ProverContext still needs to be queried to extract model data.
+        Pool.CheckerDied();
+        return;
+      }
 
       status = CheckerStatus.Idle;
       var becameIdle = thmProver.GoBackToIdle().Wait(TimeSpan.FromMilliseconds(100));
