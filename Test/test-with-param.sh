6fb559bd3726352dcb7c188a9461aab79931b441
Aaron Tomb <aarotomb@amazon.com>
2022-03-04 16:46:58 -0800

    Add script to run lit with a given Boogie param
    Recognizes the SKIP-WITH-PARAM directive in test files that indicates
    that the test should be skipped if the given parameter is specified.

---
+++ Test/test-with-param.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+    echo "Usage: test-with-params.sh <Boogie param> [additional lit params]"
+    exit 1
+fi
+
+PARAM=$1
+DIR=Test
+ALL_FILES=`lit --show-tests ${DIR} | grep -v "Available Tests" | sed -e "s/  Boogie :: /${DIR}\//"`
+TEST_FILES=`grep -L "SKIP-WITH-PARAM:.*${PARAM}" ${ALL_FILES}`
+
+shift 1
+if [ -z "${PARAM}" ] ; then
+  lit $* Test
+else
+  lit --param boogie_params="${PARAM}" $* ${TEST_FILES}
+fi
