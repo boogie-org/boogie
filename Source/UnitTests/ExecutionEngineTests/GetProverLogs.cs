6ae3f6f36dc1841b9488991d4b8a25e6a0943ac6
Remy Willems <rwillems@amazon.com>
2022-03-10 17:05:11 +0100

    Fix tests that call engine.ProcessProgram so they wait for it to finish

--- Source/UnitTests/ExecutionEngineTests/GetProverLogs.cs
+++ Source/UnitTests/ExecutionEngineTests/GetProverLogs.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Boogie;
 using NUnit.Framework;
 
@@ -9,14 +10,14 @@ namespace ExecutionEngineTests;
 
 public static class GetProverLogs
 {
-  public static string GetProverLogForProgram(ExecutionEngineOptions options, string procedure)
+  public static async Task<string> GetProverLogForProgram(ExecutionEngineOptions options, string procedure)
   {
-    var logs = GetProverLogsForProgram(options, procedure).ToList();
+    var logs = await GetProverLogsForProgram(options, procedure).ToListAsync();
     Assert.AreEqual(1, logs.Count);
     return logs[0];
   }
     
-  public static IEnumerable<string> GetProverLogsForProgram(ExecutionEngineOptions options, string procedure1)
+  public static async IAsyncEnumerable<string> GetProverLogsForProgram(ExecutionEngineOptions options, string procedure1)
   {
     using var engine = ExecutionEngine.CreateWithoutSharedCache(options);
     var defines = new List<string>() { "FILE_0" };
@@ -30,9 +31,9 @@ public static class GetProverLogs
     var temp1 = directory + "/proverLog";
     engine.Options.ProverLogFilePath = temp1;
     engine.Options.ProverOptions.Add("SOLVER=noop");
-    var success1 = engine.ProcessProgram(Console.Out, program1, "1");
+    var success1 = await engine.ProcessProgram(Console.Out, program1, "1");
     foreach (var proverFile in Directory.GetFiles(directory)) {
-      yield return File.ReadAllText(proverFile);
+      yield return await File.ReadAllTextAsync(proverFile);
     }
   }
 }
