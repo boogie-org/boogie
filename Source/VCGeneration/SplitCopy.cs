a949c6836e6f41ae7ba9b8b226a0adb73ca75b5c
Mikael Mayer <mimayere@amazon.com>
2022-03-21 23:25:48 -0500

    Removed duplicate classes

--- Source/VCGeneration/SplitCopy.cs
+++ Source/VCGeneration/SplitCopy.cs
@@ -14,57 +14,6 @@ namespace VC
   using Bpl = Microsoft.Boogie;
   using System.Threading.Tasks;
 
-  public record VCResult
-  (
-    int vcNum,
-    DateTime startTime,
-    ProverInterface.Outcome outcome,
-    TimeSpan runTime,
-    int maxCounterExamples,
-    List<Counterexample> counterExamples,
-    List<AssertCmd> asserts,
-    int resourceCount
-  ) {
-    public void ComputePerAssertOutcomes(out Dictionary<AssertCmd, ProverInterface.Outcome> perAssertOutcome,
-      out Dictionary<AssertCmd, Counterexample> perAssertCounterExamples) {
-      perAssertOutcome = new();
-      perAssertCounterExamples = new();
-      if (outcome == ProverInterface.Outcome.Valid) {
-        perAssertOutcome = asserts.ToDictionary(cmd => cmd, assertCmd => ProverInterface.Outcome.Valid);
-      } else {
-        foreach (var counterExample in counterExamples) {
-          // Only deal with the ocunter-examples that cover the asserts of this split.
-          AssertCmd underlyingAssert;
-          if (counterExample is AssertCounterexample assertCounterexample) {
-            underlyingAssert = assertCounterexample.FailingAssert;
-          } else if (counterExample is CallCounterexample callCounterexample) {
-            underlyingAssert = callCounterexample.FailingAssert;
-          } else if (counterExample is ReturnCounterexample returnCounterexample) {
-            underlyingAssert = returnCounterexample.FailingAssert;
-          } else {
-            continue;
-          }
-
-          if (!asserts.Contains(underlyingAssert)) {
-            continue;
-          }
-
-          perAssertOutcome.Add(underlyingAssert, ProverInterface.Outcome.Invalid);
-          perAssertCounterExamples.Add(underlyingAssert, counterExample);
-        }
-
-        var remainingOutcome =
-          outcome == ProverInterface.Outcome.Invalid && counterExamples.Count < maxCounterExamples
-            ? ProverInterface.Outcome.Valid
-            : outcome;
-        // Everything not listed is successful if no counter-example covers it and we did not reach the limit of counter-examples
-        foreach (var assert in asserts) {
-          perAssertOutcome.TryAdd(assert, remainingOutcome);
-        }
-      }
-    }
-  };
-
   public class Split : ProofRun
   {
       private VCGenOptions options;
