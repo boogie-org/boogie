5b44c05defe8d374cdecaaf4b6b058dee6e088f7
Sasha Fedchin <fedchina@amazon.com>
2022-07-26 17:02:38 -0700

    Fix Model Parsing

--- Source/Provers/SMTLib/SMTLibProcessTheoremProver.cs
+++ Source/Provers/SMTLib/SMTLibProcessTheoremProver.cs
@@ -1291,7 +1291,7 @@ namespace Microsoft.Boogie.SMTLib
 
     protected Model ParseErrorModel(SExpr resp)
     {
-      if (resp is null || resp.Name.Contains("error")) {
+      if (resp is null || resp.Name == "error") {
         return null;
       }
 
