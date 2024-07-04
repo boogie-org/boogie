fcd9f83b8d715be3c0278777aeedb9f5d0f6a904
Remy Willems <rwillems@amazon.com>
2024-07-04 17:10:30 +0200

    Fix broken reference

--- Test/lean-auto/test.sh
+++ Test/lean-auto/test.sh
@@ -5,5 +5,5 @@ set -e
 boogie_file=$1
 base=`basename $boogie_file .bpl`
 lean_file="$base.lean"
-dotnet ../../Source/BoogieDriver/bin/Debug/net6.0/BoogieDriver.dll /printLean:$lean_file $boogie_file
+dotnet ../../Source/BoogieDriver/bin/Debug/net8.0/BoogieDriver.dll /printLean:$lean_file $boogie_file
 lake env lean $lean_file
