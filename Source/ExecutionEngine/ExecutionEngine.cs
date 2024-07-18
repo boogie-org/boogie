233b3d24329f92f7d97bae47cbc2eba57ce4162f
Remy Willems <rwillems@amazon.com>
2024-07-18 11:41:30 +0200

    Use large stack in another place

--- Source/ExecutionEngine/ExecutionEngine.cs
+++ Source/ExecutionEngine/ExecutionEngine.cs
@@ -581,32 +581,40 @@ namespace Microsoft.Boogie
 
     private ProcessedProgram PreProcessProgramVerification(Program program)
     {
-      // Doing lambda expansion before abstract interpretation means that the abstract interpreter
-      // never needs to see any lambda expressions.  (On the other hand, if it were useful for it
-      // to see lambdas, then it would be better to more lambda expansion until after inference.)
-      if (Options.ExpandLambdas) {
-        LambdaHelper.ExpandLambdas(Options, program);
-        if (Options.PrintFile != null && Options.PrintLambdaLifting) {
-          PrintBplFile(Options.PrintFile, program, false, true, Options.PrettyPrint);
+      return LargeThreadTaskFactory.StartNew(() =>
+      {
+        // Doing lambda expansion before abstract interpretation means that the abstract interpreter
+        // never needs to see any lambda expressions.  (On the other hand, if it were useful for it
+        // to see lambdas, then it would be better to more lambda expansion until after inference.)
+        if (Options.ExpandLambdas)
+        {
+          LambdaHelper.ExpandLambdas(Options, program);
+          if (Options.PrintFile != null && Options.PrintLambdaLifting)
+          {
+            PrintBplFile(Options.PrintFile, program, false, true, Options.PrettyPrint);
+          }
         }
-      }
 
-      if (Options.UseAbstractInterpretation) {
-        new AbstractInterpretation.NativeAbstractInterpretation(Options).RunAbstractInterpretation(program);
-      }
+        if (Options.UseAbstractInterpretation)
+        {
+          new AbstractInterpretation.NativeAbstractInterpretation(Options).RunAbstractInterpretation(program);
+        }
 
-      if (Options.LoopUnrollCount != -1) {
-        program.UnrollLoops(Options.LoopUnrollCount, Options.SoundLoopUnrolling);
-      }
+        if (Options.LoopUnrollCount != -1)
+        {
+          program.UnrollLoops(Options.LoopUnrollCount, Options.SoundLoopUnrolling);
+        }
 
-      var processedProgram = Options.ExtractLoops ? ExtractLoops(program) : new ProcessedProgram(program);
+        var processedProgram = Options.ExtractLoops ? ExtractLoops(program) : new ProcessedProgram(program);
 
-      if (Options.PrintInstrumented) {
-        program.Emit(new TokenTextWriter(Options.OutputWriter, Options.PrettyPrint, Options));
-      }
+        if (Options.PrintInstrumented)
+        {
+          program.Emit(new TokenTextWriter(Options.OutputWriter, Options.PrettyPrint, Options));
+        }
 
-      program.DeclarationDependencies = Prune.ComputeDeclarationDependencies(Options, program);
-      return processedProgram;
+        program.DeclarationDependencies = Prune.ComputeDeclarationDependencies(Options, program);
+        return processedProgram;
+      }).Result;
     }
 
     private ProcessedProgram ExtractLoops(Program program)
