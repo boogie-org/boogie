63a1088fe468ee1fd826901e0f2f5631b838cd7d
Fabio Madge <fmadge@amazon.com>
2024-10-13 23:47:52 +0200

    remove noop

--- Source/VCGeneration/Splits/ManualSplitFinder.cs
+++ Source/VCGeneration/Splits/ManualSplitFinder.cs
@@ -22,7 +22,7 @@ public static class ManualSplitFinder {
     
       var splitParts = SplitAttributeHandler.GetParts(withoutIsolatedAssertions);
       var splits = isolatedJumps.Concat(isolatedAssertions).Concat(splitParts).Where(s => s.Asserts.Any()).ToList();
-      return splits.ToList().Any() ? splits.ToList() : new List<ManualSplit> { focussedPart };
+      return splits.Any() ? splits : new List<ManualSplit> { focussedPart };
     });
     return result;
   }
