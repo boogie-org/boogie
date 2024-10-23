5dc52d87d24bdae1f1daae7246c8b612288feb6c
Remy Willems <rwillems@amazon.com>
2024-10-23 15:00:43 +0200

    Add missing optimize

--- Source/VCGeneration/Splits/SplitAttributeHandler.cs
+++ Source/VCGeneration/Splits/SplitAttributeHandler.cs
@@ -62,7 +62,10 @@ class SplitAttributeHandler {
     return vcs;
 
     ManualSplit CreateVc(IImplementationPartOrigin token, List<Block> blocks) {
-      return new ManualSplit(partToSplit.Options, () => blocks, 
+      return new ManualSplit(partToSplit.Options, () => {
+          BlockTransformations.Optimize(blocks);
+          return blocks;
+        }, 
         partToSplit.parent, partToSplit.Run, token);
     }
   }
