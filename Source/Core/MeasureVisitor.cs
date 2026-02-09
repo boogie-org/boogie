f8d57dfef3e7c792b3dd206423639ced7643d2c2
Namratha <namrathag26@gmail.com>
2026-02-09 18:26:33 +0100

    clean up

--- Source/Core/MeasureVisitor.cs
+++ Source/Core/MeasureVisitor.cs
@@ -1,95 +1,62 @@
 using System.Collections.Generic;
-using System;
 using Microsoft.BaseTypes;
 
-namespace Microsoft.Boogie;
-
-public class MeasureVisitor : StandardVisitor
+namespace Microsoft.Boogie
 {
-  public override Procedure VisitProcedure(Procedure node)
+  public class MeasureVisitor : StandardVisitor
   {
-    // find all measure and add precondition
-    foreach (var dec in node.Measure)
-    { 
-      var condition = dec.Condition;
-      Expr zero = new LiteralExpr(Token.NoToken, BigNum.ZERO);
-      var condition2 = Expr.Gt(condition, zero);
-      var req = new Requires(Token.NoToken, true, condition2, "hello");
-      node.Requires.Add(req);
-    }
-    return base.VisitProcedure(node);
-  }
-
-  
-  public override Implementation VisitImplementation(Implementation node)
-  {
-    // procedure with at least one measure, visit it's body and add assertions
-    // look up node.Proc 
-    
-    var newBlockList = new List<Block>();
-    
-    foreach(var block in node.Blocks)
+    public override Procedure VisitProcedure(Procedure node)
     {
-      var newblock = new Block(Token.NoToken, null);
-      foreach(var cmd in block.Cmds)
+      foreach (var mes in node.Measure)
       {
-        if (cmd is CallCmd callCmd)
+        var condition = mes.Condition;
+        Expr zero = new LiteralExpr(Token.NoToken, BigNum.ZERO);
+        var condition2 = Expr.Gt(condition, zero);
+        var req = new Requires(Token.NoToken, true, condition2, "");
+        node.Requires.Add(req);
+      }
+
+      return base.VisitProcedure(node);
+    }
+
+    public override Implementation VisitImplementation(Implementation node)
+    {
+      var newBlockList = new List<Block>();
+
+      foreach (var block in node.Blocks)
+      {
+        var newBlock = new Block(Token.NoToken, null);
+
+        foreach (var cmd in block.Cmds)
         {
-        var count =0;
-        foreach(var dec2 in callCmd.Proc.Measure)
+          if (cmd is CallCmd callCmd)
           {
-            AssertCmd ass = new AssertCmd(Token.NoToken, Expr.Lt(dec2.Condition, node.Proc.Measure[count].Condition));
-            newblock.Cmds.Add(ass);
-            count++;
+            var count = 0;
+
+            foreach (var mes in callCmd.Proc.Measure)
+            {
+              var ass = new AssertCmd(
+                Token.NoToken,
+                Expr.Lt(mes.Condition, node.Proc.Measure[count].Condition)
+              );
+
+              newBlock.Cmds.Add(ass);
+              count++;
+            }
+          }
         }
-        }
+
+        newBlockList.Add(newBlock);
+      }
+
+      node.Blocks = newBlockList;
+      return base.VisitImplementation(node);
     }
-      newBlockList.Add(newblock);
-    }
 
-    node.Blocks = newBlockList;
-    Console.WriteLine("hello");
-
-
-
-    Console.WriteLine(node);
-    return base.VisitImplementation(node);
-  }
-
-  // public void VisitImplementation(Implementation impl)
-  // {
-  //   throw new NotImplementedException();
-  // }
-
-  public override Cmd VisitCallCmd(CallCmd node)
+    public override Cmd VisitCallCmd(CallCmd node)
     {
       VisitProcedure(node.Proc);
       return base.VisitCallCmd(node);
     }
-
-  //  private AddCheckers(CivlTypeChecker civlTypeChecker, List<Declaration> decls)
-  //   {
-
-
-  //   }
-
-  // public override List<Cmd> VisitCmdSeq(List<Cmd> cmdSeq)
-  // {
-  //   // descends into VisitCallCmd;
-  //   // introduce newCmdSeq in which we create a newCmdSeq = same ;insert assertion; insert call;
-  //   // return newCmdSeq
-  //   return base.VisitCmdSeq(cmdSeq);
-  // }
+  }
 }
-
-/*
-A 
-measure x, y;
-{
-
-call B;
-}
-
-B measure y
-
-*/
